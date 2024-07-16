d489cfc3b34e036296fb9e63a30cc77054260a6b
Hieu Chu <minhieuchu@gmail.com>
2024-07-16 13:43:45 +0700

    Create UsersConnection record when sending message, using Redis for caching

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -9,6 +9,24 @@ class DirectMessagesController < ApplicationController
 
   # POST /messages
   def create
+    sender_id = message_params[:sender_id]
+    recipient_id = message_params[:recipient_id]
+    redis_connection_key = "users_connection:#{[sender_id, recipient_id].sort.join(":")}"
+    redis_connection_value = $redis.get(redis_connection_key)
+
+    unless redis_connection_value
+      existing_connection = (UsersConnection.find_by user_a_id: sender_id, user_b_id: recipient_id) ||
+                            (UsersConnection.find_by user_a_id: recipient_id, user_b_id: sender_id)
+
+      if existing_connection.nil?
+        new_connection = UsersConnection.create user_a_id: sender_id, user_b_id: recipient_id
+      end
+
+      if existing_connection || new_connection.persisted?
+        $redis.set(redis_connection_key, 1)
+      end
+    end
+
     @message = DirectMessage.new(message_params)
 
     if @message.save
